0cfd5ee74b5dc8092c1a67a5a45336a1a3e4a140
Your Name <you@example.com>
2024-05-25 11:49:30 -0300

    add some seeds

--- src/seed/seed.js
+++ src/seed/seed.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -34,17 +35,20 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var _this = this;
+Object.defineProperty(exports, "__esModule", { value: true });
+var bcryptjs_1 = require("bcryptjs");
 var PrismaClient = require("@prisma/client").PrismaClient;
 var prisma = new PrismaClient();
 function main() {
     return __awaiter(this, void 0, void 0, function () {
-        var barberShop;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
+        var barberShop, barbers, _a, _b, _c, services;
+        var _d, _e, _f, _g;
+        return __generator(this, function (_h) {
+            switch (_h.label) {
                 case 0: return [4 /*yield*/, prisma.barberShop.create({
                         data: {
                             name: 'Barbearia Nova',
+                            imutable_id: '1',
                             open_at: '07:00',
                             close_at: '20:00',
                             picture: 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
@@ -52,15 +56,77 @@ function main() {
                         }
                     })];
                 case 1:
-                    barberShop = _a.sent();
-                    console.log(barberShop);
+                    barberShop = _h.sent();
+                    _b = (_a = prisma.barber).createMany;
+                    _d = {};
+                    _e = {
+                        name: 'Babeiro 1',
+                        email: 'babeiro1@gmail.com'
+                    };
+                    return [4 /*yield*/, (0, bcryptjs_1.hash)('12345678', 10)];
+                case 2:
+                    _c = [
+                        (_e.password = _h.sent(),
+                            _e.phone = '11912345678',
+                            _e.picture = 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
+                            _e)
+                    ];
+                    _f = {
+                        name: 'Babeiro 2',
+                        email: 'babeiro2@gmail.com'
+                    };
+                    return [4 /*yield*/, (0, bcryptjs_1.hash)('12345678', 10)];
+                case 3:
+                    _c = _c.concat([
+                        (_f.password = _h.sent(),
+                            _f.phone = '21987654321',
+                            _f.picture = 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
+                            _f)
+                    ]);
+                    _g = {
+                        name: 'Babeiro 3',
+                        email: 'babeiro3@gmail.com'
+                    };
+                    return [4 /*yield*/, (0, bcryptjs_1.hash)('12345678', 10)];
+                case 4: return [4 /*yield*/, _b.apply(_a, [(_d.data = _c.concat([
+                            (_g.password = _h.sent(),
+                                _g.phone = '31923456789',
+                                _g.picture = 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
+                                _g)
+                        ]),
+                            _d)])];
+                case 5:
+                    barbers = _h.sent();
+                    return [4 /*yield*/, prisma.service.createMany({
+                            data: [
+                                {
+                                    name: 'Corte Navalhado',
+                                    price: 35
+                                },
+                                {
+                                    name: 'Corte Social',
+                                    price: 25
+                                },
+                                {
+                                    name: 'Barba',
+                                    price: 15
+                                },
+                                {
+                                    name: 'Sobrancelha',
+                                    price: 10
+                                }
+                            ]
+                        })];
+                case 6:
+                    services = _h.sent();
+                    console.log(barberShop, barbers, services);
                     return [2 /*return*/];
             }
         });
     });
 }
 main()
-    .then(function () { return __awaiter(_this, void 0, void 0, function () {
+    .then(function () { return __awaiter(void 0, void 0, void 0, function () {
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0: return [4 /*yield*/, prisma.$disconnect()];
@@ -70,7 +136,7 @@ main()
         }
     });
 }); })
-    .catch(function (e) { return __awaiter(_this, void 0, void 0, function () {
+    .catch(function (e) { return __awaiter(void 0, void 0, void 0, function () {
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,3 +1,5 @@
+import { hash } from "bcryptjs"
+
 const { PrismaClient } = require("@prisma/client") 
 const prisma = new PrismaClient()
 async function main() {
@@ -11,7 +13,54 @@ async function main() {
       isOpen: false
     }
   })
-  console.log(barberShop)
+
+  const barbers = await prisma.barber.createMany({
+    data: [
+      {
+        name: 'Babeiro 1',
+        email: 'babeiro1@gmail.com',
+        password: await hash('12345678', 10),
+        phone: '11912345678',
+        picture: 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
+      },
+      {
+        name: 'Babeiro 2',
+        email: 'babeiro2@gmail.com',
+        password: await hash('12345678', 10),
+        phone: '21987654321',
+        picture: 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
+      },
+      {
+        name: 'Babeiro 3',
+        email: 'babeiro3@gmail.com',
+        password: await hash('12345678', 10),
+        phone: '31923456789',
+        picture: 'https://files.edgestore.dev/sbc5p8nrz7wz0e65/publicFiles/_public/82a42e49-9c3e-438e-8ce1-e4e9a4717ce8.jpg',
+      }
+    ]
+  })
+
+  const services = await prisma.service.createMany({
+    data: [
+      {
+        name: 'Corte Navalhado',
+        price: 35
+      },
+      {
+        name: 'Corte Social',
+        price: 25
+      },
+      {
+        name: 'Barba',
+        price: 15
+      },
+      {
+        name: 'Sobrancelha',
+        price: 10
+      }
+    ]
+  })
+  console.log(barberShop, barbers, services)
 }
 main()
   .then(async () => {
